728441e055426d4e3cab178a676ce43fdcaab14f
RaimondasA <ra.alisa.123@gmail.com>
2018-12-17 19:15:01 +0200

    getFxRateForCurrency moved to separate file

--- src/app/containers/Main.js
+++ src/app/containers/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+
+import { getFxRateForCurrency } from '../server/lbApi';
 import MainForm from '../components/mainForm/MainForm';
 
 class Main extends Component {
@@ -13,6 +14,8 @@ class Main extends Component {
       currencyRate2: '0',
       difference: '0'
     };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleInputChange = event => {
@@ -20,49 +23,29 @@ class Main extends Component {
     this.setState({ [name]: value });
   };
 
-  handleSubmit = event => {
+  async handleSubmit(event) {
     event.preventDefault();
 
-    const getFxRateForCurrency = (currency, date1, date2) => {
-      axios
-        .all([
-          axios.get(
-            `https://cors.io/?http://old.lb.lt//webservices/fxrates/FxRates.asmx/getFxRatesForCurrency?tp=EU&ccy=${currency}&dtFrom=${date1}&dtTo=${date1}`
-          ),
-          axios.get(
-            `https://cors.io/?http://old.lb.lt//webservices/fxrates/FxRates.asmx/getFxRatesForCurrency?tp=EU&ccy=${currency}&dtFrom=${date2}&dtTo=${date2}`
-          )
-        ])
-        .then(
-          axios.spread((response1, response2) => {
-            const value1 = getElementValueFromResponse(response1, 'Amt');
-            const value2 = getElementValueFromResponse(response2, 'Amt');
-            this.setState({ currencyRate1: value1 });
-            this.setState({ currencyRate2: value2 });
-            this.setState({ difference: (value2 - value1).toFixed(4) });
-          })
+    const getValuesFromServer = new Promise(resolve => {
+      resolve(
+        getFxRateForCurrency(
+          this.state.currency,
+          this.state.from,
+          this.state.to
         )
-        .catch(error => {
-          if (error.response) {
-            alert(
-              'Problem with Lb.lt server responce. Please try another time or conect directly' +
-                'https://www.lb.lt/lt/kasdien-skelbiami-euro-ir-uzsienio-valiutu-santykiai-skelbia-europos-centrinis-bankas'
-            );
-          } else if (error.request) {
-            alert('The request was made but no response was received');
-          } else {
-            alert('Please put the valid data');
-          }
-        });
-    };
+      );
+    });
 
-    const getElementValueFromResponse = (response, element) => {
-      const xmlDoc = new DOMParser().parseFromString(response.data, 'text/xml');
-      return xmlDoc.getElementsByTagName(element)[1].textContent;
-    };
-
-    getFxRateForCurrency(this.state.currency, this.state.from, this.state.to);
-  };
+    getValuesFromServer.then(responseValues => {
+      this.setState({
+        currencyRate1: responseValues.currencyRate1,
+        currencyRate2: responseValues.currencyRate2,
+        difference: (
+          responseValues.currencyRate2 - responseValues.currencyRate1
+        ).toFixed(4)
+      });
+    });
+  }
 
   render() {
     return (
---
+++ src/app/server/lbApi.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+export const getFxRateForCurrency = async (currency, date1, date2) => {
+  let responseValues = {};
+  await axios
+    .all([
+      axios.get(
+        `https://cors.io/?http://old.lb.lt//webservices/fxrates/FxRates.asmx/getFxRatesForCurrency?tp=EU&ccy=${currency}&dtFrom=${date1}&dtTo=${date1}`
+      ),
+      axios.get(
+        `https://cors.io/?http://old.lb.lt//webservices/fxrates/FxRates.asmx/getFxRatesForCurrency?tp=EU&ccy=${currency}&dtFrom=${date2}&dtTo=${date2}`
+      )
+    ])
+    .then(
+      axios.spread((response1, response2) => {
+        const value1 = getElementValueFromResponse(response1, 'Amt');
+        const value2 = getElementValueFromResponse(response2, 'Amt');
+        responseValues = {
+          currencyRate1: value1,
+          currencyRate2: value2
+        };
+      })
+    )
+    .catch(error => {
+      if (error.response) {
+        alert(
+          'Problem with Lb.lt server responce. Please try another time or conect directly' +
+            'https://www.lb.lt/lt/kasdien-skelbiami-euro-ir-uzsienio-valiutu-santykiai-skelbia-europos-centrinis-bankas'
+        );
+      } else if (error.request) {
+        alert('The request was made but no response was received');
+      } else {
+        alert('Please put the valid data');
+      }
+    });
+  return responseValues;
+};
+
+const getElementValueFromResponse = (response, element) => {
+  const xmlDoc = new DOMParser().parseFromString(response.data, 'text/xml');
+  return xmlDoc.getElementsByTagName(element)[1].textContent;
+};
